6e7baf582d796f8a850396ea14341b4e999b970f
Jason-Vaughan <95194903+Jason-Vaughan@users.noreply.github.com>
2025-10-01 20:27:25 -0700

    fix: clean vite config, deploy build to docs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // IMPORTANT for GitHub Pages under https://jason-vaughan.github.io/jasonvaughan.com/
+  base: '/jasonvaughan.com/',
 })
+
